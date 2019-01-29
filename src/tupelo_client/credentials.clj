5eb495ec10c9b5edfa23f8161c6e32aa9ee0e113
Wes Morgan <wesmorgan@icloud.com>
2019-01-29 18:24:56 -0500

    Actually build Credentials

--- src/tupelo_client/credentials.clj
+++ src/tupelo_client/credentials.clj
@@ -5,7 +5,8 @@
 (defn build [wallet-name pass-phrase]
   (-> (TupeloRpc$Credentials/newBuilder)
       (.setWalletName wallet-name)
-      (.setPassPhrase pass-phrase)))
+      (.setPassPhrase pass-phrase)
+      .build))
 
 (defn set [obj wallet-name pass-phrase]
   (let [credentials (build wallet-name pass-phrase)]
